de291b66eb7ff726af5b20cce46d82f15b12b38b
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-08-27 14:02:57 -0300

    .

---
+++ teste/src/App.js
@@ -0,0 +1,14 @@
+/*import './App.css';*/
+import './components/atvreactads/AtvReactAds.css';
+import AtvReactAds from './components/atvreactads/AtvReactAds';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AtvReactAds/>
+    </div>
+  );
+}
+
+export default App;
---
+++ teste/src/components/atvreactads/AtvReactAds.js
@@ -0,0 +1,12 @@
+function AtvReactAds(){
+  return(
+      <div>
+          <h1>Hello, World</h1>
+          <p title = 'Cabeça explodindo'id='grande'>&#x1F92F;</p>
+          <p>Passe o mouse por cima do emoji para exibir o nome dele</p> <br/>
+          <p><span title = 'Rosto levemente sorridente'className="emo">&#x1F642;</span> <span title='Cara de chapéu de cowboy'className="emo">&#x1F920;</span> <span title = 'coelho'className="emo">&#x1F407;</span></p><br/>
+      </div>
+  )
+}
+
+export default AtvReactAds;
